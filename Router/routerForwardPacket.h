5210c8f01c760df7fbdc764946ab43ccc54aad87
YsingYang <ysingemail@gmail.com>
2017-06-06 23:06:27 +0800

    [增加] Beacon帧伪造

--- Router/routerForwardPacket.h
+++ Router/routerForwardPacket.h
@@ -7,6 +7,9 @@
 #ifndef  ROUTEFORWARDPACKET_H
 #define  ROUTEFORWARDPACKET_H
 
+#define __SENDFAKERTS__
+#define __SENDFAKEBEACON__
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -19,6 +22,7 @@
 #include <linux/if_ether.h>
 #include <netpacket/packet.h>
 #include<net/if.h>
+#include <assert.h>
 
 #include "ieee80211.h"
 #include "ieee80211_radiotap.h"
@@ -28,14 +32,22 @@
 int recvRTSFrame = 0;
 uint32_t FakeRTSLength;
 
+int recvBeaconFrame = 0;
+uint32_t  FakeBeaconLength;
+
 u_char FakeRTSFrame[128];//一般RTS
 u_char FakeBeaconFrame[1024];
+
+char SSID_1[7] = "OpenWrt";
+uint32_t ssidLength;
 
 int sockFD;
 int broadcastFD;
 struct sockaddr_in clientAddr;
 
 void sendRTSFrame();
+void initBeaconPacket(const u_char* data, int packetLength, int radiotapLength);
+void sendBeaconFrame();
 
 void loopFunction(u_char *user, const struct pcap_pkthdr *packetHeader,const u_char *packetData){
     uint32_t packetLength = packetHeader -> caplen;
@@ -46,6 +58,8 @@ void loopFunction(u_char *user, const struct pcap_pkthdr *packetHeader,const u_c
         return;
     }
     struct ieee80211_hdr *hdr = (struct ieee80211_hdr* )(packetData + rtLength);
+
+    #ifdef __SENDFAKERTS__
     if(1 == ieee80211_is_rts(hdr->frame_control) && recvRTSFrame == 0){ //未拷贝
         memcpy(FakeRTSFrame, packetData, packetLength);
         FakeRTSLength = packetLength;
@@ -54,12 +68,22 @@ void loopFunction(u_char *user, const struct pcap_pkthdr *packetHeader,const u_c
         memcpy(rtsBody->ra, "\xd0\xa6\x37\x0e\x12\x75", 6);
         memcpy(rtsBody->ta, "\xec\x17\x2f\x2d\xb6\xb9", 6);
     }
+    #endif // __SENDFAKEBEACON__
+
+    #ifdef __SENDFAKEBEACON__
+    if(0 == recvBeaconFrame && 1 == ieee80211_is_beacon(hdr->frame_control)){ //Beacon帧未拷贝
+        initBeaconPacket(packetData, packetLength, rtLength);
+    }
+    #endif // __SENDFAKEBEACON__
 
     if(sendto(sockFD,  packetData, packetLength, 0, (struct sockaddr*)(&clientAddr), sizeof(clientAddr)) < 0){
     	perror("send to error ");
     }
     if(recvRTSFrame){
         sendRTSFrame(sockFD, FakeRTSFrame, FakeRTSLength);
+    }
+    if(recvBeaconFrame){
+        sendBeaconFrame(sockFD, FakeBeaconFrame, FakeBeaconLength);
     }
 }
 
@@ -104,5 +128,44 @@ inline void sendRTSFrame(){
     if(write(broadcastFD, FakeRTSFrame, FakeRTSLength) < 0){
         perror("send 80211 packet error");
     }
+}
+
+inline void sendBeaconFrame(){
+    if(write(broadcastFD, FakeBeaconFrame, FakeBeaconLength) < 0){
+        perror("send 80211Beacon packet error");
+    }
+}
+
+void initBeaconPacket(const u_char* data, int packetLength, int radiotapLength){
+    memcpy(FakeBeaconFrame, data, packetLength); //先将帧memcpy一遍, 再进行修改
+    FakeBeaconLength = packetLength;
+    struct ieee80211_mgmt* mgmt = (struct ieee80211_mgmt*)(FakeBeaconFrame + radiotapLength);
+    struct ieee80211_mgmt* originalMgmt = (struct ieee80211_mgmt*)(data + radiotapLength);
+
+    memcpy(mgmt->sa, "\xec\x17\x2f\x2d\xb6\xb8", 6);//伪造Beacon的AP
+    ssidLength =  strlen(SSID_1);//这样会不会有问题?
+    printf("ssidLength = %d \n", ssidLength);
+
+    struct ieee80211_ie *ie = (struct ieee80211_ie*)mgmt->u.beacon.variable;
+    struct ieee80211_ie *originalIe = (struct ieee80211_ie*)originalMgmt->u.beacon.variable;
+
+    uint32_t remainOffset = packetLength - radiotapLength; //两者共用一个remainoffset
+    int modifyFlag = 1;
+    while(remainOffset > FCS_LEN && modifyFlag){
+        switch( ie->id ){
+            case WLAN_EID_SSID:
+                assert(ssidLength < IEEE80211_MAX_SSID_LEN);
+                FakeBeaconLength +=  ssidLength - ie->len;
+                ie->len = ssidLength;//需要修改长度
+                memcpy((void*)(ie->data), SSID_1, ssidLength);
+                modifyFlag = 0;//已修改
+                break;
+        }
+        ie = (struct ieee80211_ie*)((u_char*)ie + ie->len + 2);
+        originalIe = (struct ieee80211_ie*)((u_char*)originalIe + originalIe->len + 2);
+        remainOffset -= originalIe->len - 2;
+    }
+    memcpy((void*)ie, (void*)originalIe, remainOffset);
+    recvBeaconFrame = 1;//修改完成, 表示以伪造出一个Beacon帧
 }
 #endif
